1b406f02d6f86e9d496d704fdc6c07c16c8bd9be
F1dg3t <57208077+F1dg3tXD@users.noreply.github.com>
2024-04-21 19:29:58 -0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,24 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
     checkServerStatus('X');
     checkServerStatus('Y');
-    checkServerStatus('Z');
+    // checkServerStatus('Z');
 });
 
 function checkServerStatus(serverName) {
-    var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    var serverUrl = 'http://f1dg3t-' + serverName.toLowerCase() + '.duckdns.org';
-    var finalUrl = proxyUrl + serverUrl;
+    var url = 'http://f1dg3t-' + serverName.toLowerCase() + '.duckdns.org/' + serverName.toLowerCase() + '/index.html';
     var serverDiv = document.getElementById('server' + serverName.toUpperCase());
     var statusParagraph = serverDiv.querySelector('.status');
 
     // Try to fetch the server URL with a GET request
-    fetch(finalUrl)
+    fetch(url)
         .then(response => {
-            if (!response.ok) {
+            if (response.ok) {
+                statusParagraph.textContent = 'Online';
+                statusParagraph.style.color = 'green';
+            } else {
                 throw new Error('Network response was not ok');
             }
-            statusParagraph.textContent = 'Online';
-            statusParagraph.style.color = 'green';
         })
         .catch(error => {
             statusParagraph.textContent = 'Offline';
